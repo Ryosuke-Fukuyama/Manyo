d129a11c00478a53cdd9b0657f51d0b171bd7628
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-05-07 21:24:02 +0900

    制約マイグレーション

--- db/migrate/20210507054036_create_tasks.rb
+++ db/migrate/20210507054036_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :name
-      t.text :content
+      t.string :name, null: false
+      t.text :content, null: false
 
       t.timestamps
     end
