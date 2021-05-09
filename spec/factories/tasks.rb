e43e8fe0d10bc02739779b3bb9c3ea28afb88d1b
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-05-09 22:54:08 +0900

    system spec追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,4 +1,7 @@
+# 「FactoryBotを使用します」という記述
 FactoryBot.define do
+  # 作成するテストデータの名前を「task」とします
+  # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
   factory :task do
     name { 'Factoryで作ったデフォルトのタイトル１' }
     content { 'Factoryで作ったデフォルトのコンテント１' }
